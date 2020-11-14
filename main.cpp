d209d4c528759c0201f12afa33fa4ef0af07d232
kaloka <kalokaprivat@gmail.com>
2020-11-14 21:49:13 +0100

    added glfw

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // create open gl context //
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // //
     return 0;
+
+    // make glfw window //
+    GLFWwindow * glfwWindow = glfwCreateWindow(800, 600, "hangman", nullptr, nullptr);
 }
