b81a8a579024cc29fb9c8779ec2d6d5f15bfe97e
kaloka <kalokaprivat@gmail.com>
2020-11-14 21:27:52 +0100

    added glfw

--- cmake-build-debug/CMakeFiles/OGLhangman.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/OGLhangman.dir/DependInfo.cmake
@@ -1,8 +1,20 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "C"
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kaloka/CLionProjects/OGLhangman/src/glad.c" "/home/kaloka/CLionProjects/OGLhangman/cmake-build-debug/CMakeFiles/OGLhangman.dir/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "../lib/glfw/include"
+  )
 set(CMAKE_DEPENDS_CHECK_CXX
   "/home/kaloka/CLionProjects/OGLhangman/main.cpp" "/home/kaloka/CLionProjects/OGLhangman/cmake-build-debug/CMakeFiles/OGLhangman.dir/main.cpp.o"
   )
@@ -10,10 +22,14 @@ set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  "../lib/glfw/include"
   )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/kaloka/CLionProjects/OGLhangman/cmake-build-debug/lib/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
--- cmake-build-debug/CMakeFiles/OGLhangman.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/OGLhangman.dir/cmake_clean.cmake
@@ -1,10 +1,11 @@
 file(REMOVE_RECURSE
   "CMakeFiles/OGLhangman.dir/main.cpp.o"
+  "CMakeFiles/OGLhangman.dir/src/glad.c.o"
   "OGLhangman"
   "OGLhangman.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang C CXX)
   include(CMakeFiles/OGLhangman.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
