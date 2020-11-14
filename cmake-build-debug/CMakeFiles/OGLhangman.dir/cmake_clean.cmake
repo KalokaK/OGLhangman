6ea4ab0efb75b7e189ef92ade2cd9e8b4188784f
kaloka <kalokaprivat@gmail.com>
2020-11-14 21:02:44 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/OGLhangman.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OGLhangman.dir/main.cpp.o"
+  "OGLhangman"
+  "OGLhangman.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OGLhangman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
