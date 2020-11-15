501c292d4ee86663ed76d8df55749f20327913db
kaloka <kalokaprivat@gmail.com>
2020-11-15 12:47:50 +0100

    added testing framework

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -47,6 +47,11 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/home/kaloka/CLionProjects/OGLhangman/cmake-build-debug/lib/glfw/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/home/kaloka/CLionProjects/OGLhangman/cmake-build-debug/test/Google_tests/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
