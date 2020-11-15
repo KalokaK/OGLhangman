0c789bf64f2a38156af7d3465478ac89d41c5b00
kaloka <kalokaprivat@gmail.com>
2020-11-15 12:29:01 +0100

    work in progress event system

--- main.cpp
+++ main.cpp
@@ -33,6 +33,8 @@ int main() {
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, helpers::framebufferSizeCallback);
     // //
+    auto * event = new events::event<GLFWwindow *, int, int>();
+    *event += events::handler(helpers::framebufferSizeCallback);
 
     // main game loop //
     while(!glfwWindowShouldClose(window))
